7fbd318c10f8badd77e5e717e4b6440ba86e1fa3
adrake333 <adrake333@gmail.com>
2026-05-22 12:24:31 -0500

    started pokecache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,22 @@
+package pokecache
+
+
+
+
+import (
+	"time"
+)
+
+
+
+
+type Cache struct {
+	entries		map[string]cacheEntry
+	mu		sync.Mutex
+}
+
+
+type cacheEntry struct {
+	createdAt	time.Time
+	val		[]byte
+}
