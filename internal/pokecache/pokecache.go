38b09ac9371aaa40f630c7863ab3e3a15b8350f1
adrake333 <adrake333@gmail.com>
2026-05-22 15:02:04 -0500

    created pokecache methods and test

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -5,6 +5,7 @@ package pokecache
 
 import (
 	"time"
+	"sync"
 )
 
 
@@ -19,4 +20,49 @@ type Cache struct {
 type cacheEntry struct {
 	createdAt	time.Time
 	val		[]byte
+}
+
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		entries: make(map[string]cacheEntry),
+	}
+	go c.reapLoop(interval)
+	return c
+}
+
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val: val,
+	}
+}
+
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.entries[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+	
+}
+
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.mu.Lock()
+		for key, value := range c.entries {
+			if time.Since(value.createdAt) > interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mu.Unlock()
+	}
 }
